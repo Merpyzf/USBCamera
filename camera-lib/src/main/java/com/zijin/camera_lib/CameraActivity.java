93dbe1138ed3175961cb3534ae303033ee9b2c69
wangke <1052060838@qq.com>
2020-10-08 12:01:49 +0800

    init project

---
+++ camera-lib/src/main/java/com/zijin/camera_lib/CameraActivity.java
@@ -0,0 +1,16 @@
+package com.zijin.camera_lib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CameraActivity extends AppCompatActivity {
+
+    public static void start() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
